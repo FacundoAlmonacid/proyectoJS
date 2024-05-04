9bf809b293813194ad2fc81df08b68adfa159001
Facundo Almonacid <facundoalmonacid91@gmail.com>
2024-05-04 19:25:57 -0300

    reformas

--- js/main.js
+++ js/main.js
@@ -3,116 +3,95 @@ class Metales {
     this.nombre = nombre;
     this.precio = precio;
     this.presupuestoParcial = 0;
-    this.presupuestoTotal = 0;
-  }
-
-  presupuestoParcialMetales() {
-    let confirmacion = prompt(`El precio de ${this.nombre} es de ${this.precio}\nDesea continuar? si/no`);
-
-    while (confirmacion != "si" && confirmacion != "no") {
-      alert("ingrese si / no")
-      confirmacion = prompt(`El precio de ${this.nombre} es de ${this.precio}\nDesea continuar? si/no`);
-
-    }
-    if (confirmacion === "si") {
-      let cantMetal = parseInt(prompt(`Ingrese la cantidad en kilos de ${this.nombre} que desee tasar`));
-      this.presupuestoParcial = this.precio * cantMetal;
-      alert(`Tu presupuesto de ${this.nombre} fue de ${this.presupuestoParcial}`);
-      this.presupuestoTotal += this.presupuestoParcial
-      presupuestos.push(this.presupuestoParcial)
-    }
 
   }
+
+  //Metodos
 
 
 }
 
 const metalesArray = [
-  new Metales("aluminio", 4500),//0
-  new Metales("cobre", 7000),   //1
-  new Metales("plomo", 2000),   //2
+  new Metales("aluminio", 4500),
+  new Metales("cobre", 7000),
+  new Metales("plomo", 2000),
+  new Metales("hierro", 2000),
+  new Metales("bronce", 2000)
 ];
 
 const presupuestos = []
 
 
-function verificacionIngreso() {
-  let ingreso;
-  do {
-    ingreso = prompt("Hola bienvenidos a reciclArg\n¿Desea reciclar algun producto?").toLowerCase();
-    if (ingreso !== "si" && ingreso !== "no") {
-      alert("Opción incorrecta, ingrese si/no");
-    }
-  } while (ingreso !== "si" && ingreso !== "no");
-  return ingreso;
+
+
+
+
+
+
+
+
+//DOM
+
+const divMetales = document.getElementById("metales")
+
+
+const divtotales = document.getElementById("totales")
+
+
+//Funciones
+
+function agregarPresupuesto(metal, cantidadTasar) {
+  const presupuestoParcial = metal.precio * cantidadTasar
+  presupuestos.push(presupuestoParcial)
+
+  const indicePresupuesto=presupuestos.findIndex((el)=>{
+    return 
+  } )
 }
 
-function pedirNombre() {
-  let nombre;
-  do {
-    nombre = prompt("Ingrese su nombre");
-    if (nombre === "") {
-      alert("Ingrese un nombre correcto");
-    }
-  } while (nombre === "");
-  return nombre;
-}
 
-function eliminarPresupuesto() {
-  let opcionesEliminar = "";
-  for (let i = 0; i < presupuestos.length; i++) {
-    opcionesEliminar += `${i}.${presupuestos[i]}\n`;
+
+function renderizarMetales() {
+  for (const metal of metalesArray) {
+    console.log(metal)
+
+    const tablaInicio = document.createElement("div")
+    tablaInicio.className = "tablaInicio"
+
+
+    const h2 = document.createElement("h2")
+    h2.innerText = metal.nombre
+
+    const h3 = document.createElement("h3")
+    h3.innerText = `$${metal.precio} xkg`
+
+    const inputCantidad = document.createElement("input")
+    inputCantidad.type = "number"
+    inputCantidad.placeholder = "ingrese cantidad a tasar"
+
+    const botonAgregar = document.createElement("button")
+    botonAgregar.innerText = "agregar"
+    botonAgregar.addEventListener("click", () => {
+      const cantidadTasar = parseInt(inputCantidad.value);
+
+      agregarPresupuesto(metal, cantidadTasar)
+
+
+    })
+
+
+    tablaInicio.append(h2, h3, inputCantidad, botonAgregar)
+    divMetales.append(tablaInicio)
+
   }
 
-  let indiceAEliminar = prompt(`Elige el presupuesto que desea eliminar:\n${opcionesEliminar}`);//de aca sale el numero indice
-  presupuestos.splice(indiceAEliminar, 1)
-  
 
 }
 
-//inicio programa
-
-let nombre;
-let ingreso = verificacionIngreso();
-
-if (ingreso === "si") {
-  nombre = pedirNombre();
-  let opcion;
-
-  do {
-    opcion = parseInt(prompt(`Hola ${nombre}, elige la opción:\n1. Aluminio\n2. Cobre\n3. Plomo\n4. Ver presupuesto total\n5. Eliminar presupuestos \n6. Salir`));
-
-    switch (opcion) {
-
-      case 1:
-        metalesArray[0].presupuestoParcialMetales();
-        break;
-
-      case 2:
-        metalesArray[1].presupuestoParcialMetales();
 
 
-        break;
-
-      case 3:
-        metalesArray[2].presupuestoParcialMetales();
-
-        break;
-
-      case 4:
-        const total = presupuestos.reduce((acc, el) => acc + el, 0)
 
 
-        alert(`Presupuesto total acumulado: $${total}`);
-        break;
+//inicio del programa
 
-      case 5:
-
-        eliminarPresupuesto()
-        break
-
-
-    }
-  } while (opcion !== 6);
-}
-else (alert("Muchas gracias por tu visita"))
+renderizarMetales()
